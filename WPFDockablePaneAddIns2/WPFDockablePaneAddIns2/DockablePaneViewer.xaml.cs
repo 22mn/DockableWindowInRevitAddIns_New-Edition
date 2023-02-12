b2c05046c2e74f15dc19e2ff6820b238a4074691
Min Naung <p.jean90@gmail.com>
2023-02-12 17:33:09 +0800

    add files via upload

---
+++ WPFDockablePaneAddIns2/WPFDockablePaneAddIns2/DockablePaneViewer.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System.Reflection;
+
+namespace WPFDockablePaneAddIns2
+{
+    /// <summary>
+    /// Interaction logic for DockablePaneViewer.xaml
+    /// </summary>
+    public partial class DockablePaneViewer : UserControl, IDockablePaneProvider
+    {
+        public DockablePaneViewer()
+        {
+            //initialize wpf control
+            InitializeComponent();
+
+            // navigated event register
+            // to hide unwanted JS Script pop-ups
+            wb.Navigated += (a, b) => { HideScriptErrors(wb, true); };
+        }
+
+        // callback method
+        public void HideScriptErrors(WebBrowser wb, bool hide)
+        {
+            // get webbrowser's a private field / _axIWebBrowser2
+            // field search included instance & non-public members /private field
+            FieldInfo fiComWebBrowser = typeof(WebBrowser).GetField("_axIWebBrowser2",
+                BindingFlags.Instance | BindingFlags.NonPublic);
+            if (fiComWebBrowser == null) return;
+
+            // get COM webbrowser obj
+            var objComWebBrowser = fiComWebBrowser.GetValue(wb);
+            if (objComWebBrowser == null)
+            {
+                // in case too early
+                wb.Loaded += (o, s) => HideScriptErrors(wb, hide);
+                return;
+            }
+            // invokes the specified member, using the specified binding constraints 
+            // and matching the specified argument list
+            objComWebBrowser.GetType().InvokeMember("Silent", BindingFlags.SetProperty,
+                null, objComWebBrowser, new object[] { hide });
+        }
+
+        public void SetupDockablePane(DockablePaneProviderData data)
+        {
+            // wpf object with pane's interface
+            data.FrameworkElement = this as FrameworkElement;
+            // initial state position
+            data.InitialState = new DockablePaneState
+            {
+                DockPosition = DockPosition.Tabbed,
+                TabBehind = DockablePanes.BuiltInDockablePanes.ProjectBrowser
+            };
+        }
+    }
+}
---
+++ WPFDockablePaneAddIns2/WPFDockablePaneAddIns2/MainClass.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB.Events;
+using Autodesk.Revit.Attributes;
+
+namespace WPFDockablePaneAddIns2
+{
+    public class MainClass : IExternalApplication
+    {
+        // execute when app open
+        public Result OnStartup(UIControlledApplication application)
+        {
+            // create ribbon panel (in addins tab)
+            RibbonPanel ribbonPanel = application.CreateRibbonPanel(Tab.AddIns, "TwentyTwo Sample");
+            // this assembly
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            // assembly path 
+            string assemblyPath = assembly.Location;
+
+            // Create Show Button
+            PushButton showButton = ribbonPanel.AddItem(new PushButtonData("Show Window", "Show", assemblyPath,
+                "WPFDockablePaneAddIns2.Show")) as PushButton;
+            // btn tooltip
+            showButton.ToolTip = "Show the registered dockable window.";
+            // show button icon images
+            showButton.LargeImage = GetResourceImage(assembly, "WPFDockablePaneAddIns2.Resources.show32.png");
+            showButton.Image = GetResourceImage(assembly, "WPFDockablePaneAddIns2.Resources.show16.png");
+
+            // register dockablepane
+            RegisterDockablePane(application);
+
+            // return result
+            return Result.Succeeded;
+        }
+
+        // execute when app close
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            // return result
+            return Result.Succeeded;
+        }
+        // get embedded images from assembly resources
+        public ImageSource GetResourceImage(Assembly assembly, string imageName)
+        {
+            try
+            {
+                // bitmap stream to construct bitmap frame
+                Stream resource = assembly.GetManifestResourceStream(imageName);
+                // return image data
+                return BitmapFrame.Create(resource);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        // register dockable pane 
+        public Result RegisterDockablePane(UIControlledApplication application)
+        {
+            DockablePaneViewer window = new DockablePaneViewer();
+
+            DockablePaneId dockID = new DockablePaneId(new Guid("{29F293CF-5486-45CB-8C10-60CF2A33DDAB}"));
+            try
+            {
+                application.RegisterDockablePane(dockID, "TwentyTwo DockablePane Sample",
+                    window as IDockablePaneProvider);
+                
+            }
+            catch(Exception ex)
+            {
+                TaskDialog.Show("Error Message", ex.Message);
+                return Result.Failed;
+            }
+            return Result.Succeeded;
+        }
+
+    }
+    // external command class
+    [Transaction(TransactionMode.Manual)]
+    public class Show : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            try
+            {
+                // dockable window id
+                DockablePaneId id = new DockablePaneId(new Guid("{29F293CF-5486-45CB-8C10-60CF2A33DDAB}"));
+                DockablePane dockableWindow = commandData.Application.GetDockablePane(id);
+                dockableWindow.Show();
+            }
+            catch (Exception ex)
+            {
+                // show error info dialog
+                TaskDialog.Show("Info Message", ex.Message);
+            }
+            // return result
+            return Result.Succeeded;
+        }
+    }
+}
